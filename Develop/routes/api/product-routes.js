470a66bb824e3fbf288547ec7e2b6142eb9f31ca
Gustavo Tijerino <busg26@gmail.com>
2022-04-08 15:50:37 -0500

    Updated tag route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -69,6 +69,7 @@ router.post('/', (req, res) => {
     });
 });
 
+
 // update product
 router.put('/:id', (req, res) => {
   // update product data
@@ -112,7 +113,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
